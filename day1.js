b90d33edc1e1b865767def8adc529a79ea23c21b
Kaviarasu-Ceiyone <118160152+Kaviarasu-Ceiyone@users.noreply.github.com>
2022-11-30 07:53:50 +0000

    Pending changes exported from your codespace

---
+++ day1.js
@@ -0,0 +1,12 @@
+// JavaScript Program To Print Hello World
+
+console.log("Hello World");
+
+//JavaScript Program to Add Two Numbers
+
+function sum (a,b) {
+  total = a + b;
+  console.log(total);
+}
+
+sum(2,4);
